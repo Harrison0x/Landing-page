3d61127c19aacbaeae95571e361414a45eca4f36
Harrison0x <146104280+Harrison0x@users.noreply.github.com>
2023-12-19 00:14:17 +0100

    Add files via upload

---
+++ Incomplete Portfolio/main.js
@@ -0,0 +1,7 @@
+var typed = new Typed( ".text", {
+    strings:["Technical Writer", "Blogger", "Photographer","Content-Creator", "Script-Writer", " Web Developer(in Training)"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
